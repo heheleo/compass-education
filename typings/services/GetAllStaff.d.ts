452c8b880be640c67d730bc44d37cd7f489de086
Leo <leowasntfound@gmail.com>
2025-01-12 12:44:36 +0800

    new repository setup
    - conversion to typescript with exports handled by `tsup`
    - MIT license
    - migrate to puppeteer

--- typings/services/GetAllStaff.d.ts
+++
@@ -1,8 +0,0 @@
-export = GetAllStaff;
-/**
- * Gets all the staff of the school
- * @param {String} BASEURL
- * @param {String} ALL_COOKIES
- * @returns {Promise<Response>}
- */
-declare function GetAllStaff(BASEURL: string, ALL_COOKIES: string): Promise<Response>;
--- typings/services/GetNamesByID.d.ts
+++
@@ -1,9 +0,0 @@
-export = GetNamesByID;
-/**
- * Gets the name of the current user
- * @param {String} BASEURL
- * @param {String} ALL_COOKIES
- * @param {String} USER_ID
- * @returns {Promise<Response>}
- */
-declare function GetNamesByID(BASEURL: string, ALL_COOKIES: string, USER_ID: string): Promise<Response>;
